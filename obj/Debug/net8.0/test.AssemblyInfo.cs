fe706498e398d9c8bb02bd83876dc5e582f7fa39
Web UI/UX | Blockchain | Game | Backend Engineer <backendeng0417@gmail.com>
2023-12-19 22:15:33 -0800

    adding input text

--- obj/Debug/net8.0/test.AssemblyInfo.cs
+++ obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02dd20133bed13c39b41e81a182cce6c62c9ef7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93d588acb8baf65bad2f3df3ee6ac754ca2c11b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
